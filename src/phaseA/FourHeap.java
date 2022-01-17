6435d6cfad5e7bdeb2ea55c8f891899984a08371
Cameron Richards <crichards1717@gmail.com>
2022-01-17 03:05:11 -0800

    completed FourHeap

--- src/phaseA/FourHeap.java
+++ src/phaseA/FourHeap.java
@@ -5,31 +5,11 @@ import java.util.NoSuchElementException;
 
 
 /**
- * TODO: Replace this comment with your own as appropriate.
  * Implements a 4-ary minheap using an array as the underlying data structure.
  * When data is added or removed from the heap,
  * nodes are percolated up or down to maintain heap sorted order.
  *
- * TODO: Develop appropriate JUnit tests for your FourHeap.
- * 
- * IMPORTANT NOTE:
- * 1. You MUST use the fields defined in the superclass!
- * 2. FourHeap should be a MIN-HEAP, which means the "smallest"
- *    element according to the given comparator should be at the root. It
- *    is obvious from the name of methods, deleteMin() & findMin(),
- *    which should return the minimum element determined by the given
- *    comparator. For example, the DataCountStringComparator considers
- *    the element with the highest count to be the "smallest", so when you
- *    call deleteMin() or findMin() your FourHeap should return the
- *    element with highest count.
- * 3. If you accidentally made it as MAX-HEAP instead of MIN-HEAP, it
- *    will return the "largest" element (with
- *    DataCountStringComparator, this will be the one with lowest count) when
- *    deleteMin() is called, and you are going to lose considerable
- *    amount of FourHeap points.
- * 4. For Testing: Given the same comparator, FourHeap's deleteMin()
- *    should return the same element as Java's PriorityQueue's poll().
- */
+**/
 public class FourHeap<E> extends Heap<E> {
 
 	private static final int INITIAL_CAPACITY = 8;
